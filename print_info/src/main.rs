50bfb42159390d5150b74f7abc13577d474eb9bc
zhouhaoyu <67226385+zhouhaoyiu@users.noreply.github.com>
2022-05-08 15:10:04 +0800

    some changes
    Signed-off-by: zhouhaoyu <67226385+zhouhaoyiu@users.noreply.github.com>

--- print_info/src/main.rs
+++ print_info/src/main.rs
@@ -1,18 +1,18 @@
 /// my rust study
 /// # 云香绕雪
 /// # 雪甜环云
-/// 
+///
 /// ```
 /// let res = test();
 /// ```
-/// 
-fn test () {
+///
+fn test() {
     println!("test print");
 }
 fn main() {
     test();
     let my_name: &str = "zhy";
     let my_age: u8 = 21;
-    let _birth: (u16,u8,u8) = (2000,6,24);
-    println!("myname is {0},myage is {1}", my_name, my_age);
+    let _birth: (u16, u8, u8) = (2000, 6, 24);
+    println!("my name is {0},my age is {1}", my_name, my_age);
 }
---
+++ thread/src/main.rs
@@ -0,0 +1,18 @@
+use std::thread;
+use std::time::Duration;
+
+fn spawn_function() {
+    for i in 0..5 {
+        println!("spawned thread print {}", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+}
+
+fn main() {
+    thread::spawn(spawn_function);
+
+    for i in 0..3 {
+        println!("main thread print {}", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+}
